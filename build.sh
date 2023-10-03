4795217d48ea9af31b696247f9d676a2e0ac66f4
gehrychiang <jscblack@163.com>
2023-10-03 21:53:43 +0800

    style: 修改build.sh以自动修复样式

--- build.sh
+++ build.sh
@@ -148,7 +148,6 @@ function gen_parser
 
 function build
 {
-  gen_parser
   set -- "${BUILD_ARGS[@]}"
   case "x$1" in
     xrelease)
@@ -173,8 +172,10 @@ function style
   fi
 
   # Find all source files (adjust the file extensions as needed)
-  source_files=$(find . -type f -name "*.cpp" -o -name "*.h")
+  # only format .cpp and .h files in the /src directory
 
+  source_files=$(find ./src -type f -name "*.cpp" -o -name "*.h")
+  
   # Check if there are any source files
   if [ -z "$source_files" ]; then
     echo "No source files found in the current directory."
@@ -208,6 +209,8 @@ function main
       ;;
     *)
       parse_args
+      style
+      gen_parser
       build
       try_make
       ;;
